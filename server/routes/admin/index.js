9d9659e874e12e05974a594e6e8bbf39f5f47935
舒义 <46421299+01sy@users.noreply.github.com>
2019-11-09 15:06:31 +0800

    第十五节

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,50 +1,59 @@
 module.exports = app => {
-  const express = require("express");
+  const express = require("express")
   const router = express.Router({
     mergeParams: true
   });
 
   router.post("/", async (req, res) => {
     const model = await req.Model.create(req.body);
-    res.send(model);
+    res.send(model)
   });
 
   router.put("/:id", async (req, res) => {
-    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
-    res.send(model);
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
+    res.send(model)
   });
 
   router.delete("/:id", async (req, res) => {
-    await req.Model.findByIdAndDelete(req.params.id);
+    await req.Model.findByIdAndDelete(req.params.id)
     res.send({
       success: true
-    });
-  });
+    })
+  })
 
   router.get("/", async (req, res) => {
-    const queryOptions = {};
+    const queryOptions = {}
     if (req.Model.modelName === "category") {
-      queryOptions.populate = "parent";
+      queryOptions.populate = "parent"
     }
     const items = await req.Model.find()
       .setOptions(queryOptions)
-      .limit(20);
+      .limit(20)
 
-    res.send(items);
+    res.send(items)
   });
 
   router.get("/:id", async (req, res) => {
-    const model = await req.Model.findById(req.params.id);
-    res.send(model);
-  });
+    const model = await req.Model.findById(req.params.id)
+    res.send(model)
+  })
 
   app.use(
     "/admin/api/rest/:resource",
     async (req, res, next) => {
-      const modelName = require("inflection").classify(req.params.resource);
-      req.Model = require(`../../models/${modelName}`);
-      next();
+      const modelName = require("inflection").classify(req.params.resource)
+      req.Model = require(`../../models/${modelName}`)
+      next()
     },
-    router
-  );
-};
+    router)
+
+
+    //图片上传post
+    const multer =require('multer')
+    const upload = multer({dest:__dirname + '/../../uploads'})
+    app.post('/admin/api/upload',upload.single('file'),async(req,res,next)=>{
+      const file = req.file
+      file.url = `http://localhost:3000/uploads/${file.filename}`
+      res.send(file)
+    })
+}
