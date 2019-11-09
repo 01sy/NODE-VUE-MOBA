ed0430feede4e5318e1e914e7ca85463a0fe7b4c
舒义 <46421299+01sy@users.noreply.github.com>
2019-11-09 10:56:31 +0800

    更新

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,48 +1,50 @@
 module.exports = app => {
-  const express = require('express')
+  const express = require("express");
   const router = express.Router({
     mergeParams: true
-  })
+  });
 
-  router.post('/', async (req, res) => {
-    const model = await req.Model.create(req.body)
-    res.send(model)
-  })
+  router.post("/", async (req, res) => {
+    const model = await req.Model.create(req.body);
+    res.send(model);
+  });
 
-  router.put('/:id', async (req, res) => {
-    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
-    res.send(model)
-  })
+  router.put("/:id", async (req, res) => {
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
+    res.send(model);
+  });
 
-  router.delete('/:id', async (req, res) => {
-    await req.Model.findByIdAndDelete(req.params.id)
+  router.delete("/:id", async (req, res) => {
+    await req.Model.findByIdAndDelete(req.params.id);
     res.send({
       success: true
-    })
-  })
+    });
+  });
 
-  router.get('/', async (req, res) => {
-    const queryOptions = {}
-    if(req.Model.modelName === 'category'){
-      queryOptions.populate = 'parent'
+  router.get("/", async (req, res) => {
+    const queryOptions = {};
+    if (req.Model.modelName === "category") {
+      queryOptions.populate = "parent";
     }
-    const items = await req.Model.find().setOptions(queryOptions).limit(20)
+    const items = await req.Model.find()
+      .setOptions(queryOptions)
+      .limit(20);
 
-    res.send(items)
-  })
+    res.send(items);
+  });
 
-  router.get('/:id', async (req, res) => {
-    const model = await req.Model.findById(req.params.id)
-    res.send(model)
-  })
+  router.get("/:id", async (req, res) => {
+    const model = await req.Model.findById(req.params.id);
+    res.send(model);
+  });
 
   app.use(
-    '/admin/api/rest/:resource',
+    "/admin/api/rest/:resource",
     async (req, res, next) => {
-      const modelName = require('inflection').classify(req.params.resource)
-      req.Model = require(`../../models/${modelName}`)
-      next()
+      const modelName = require("inflection").classify(req.params.resource);
+      req.Model = require(`../../models/${modelName}`);
+      next();
     },
     router
-  )
-}
+  );
+};
